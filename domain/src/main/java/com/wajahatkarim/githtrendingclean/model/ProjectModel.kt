011b464252c8c9d07a436217a407f50df8560f45
Wajahat Karim <wajahatkarim3@gmail.com>
2018-06-27 14:05:04 +0500

    added Project model for webservice

---
+++ domain/src/main/java/com/wajahatkarim/githtrendingclean/model/ProjectModel.kt
@@ -0,0 +1,6 @@
+package com.wajahatkarim.githtrendingclean.model
+
+data class ProjectModel (val id: String, val name: String, val fullName: String,
+                         val starCount: String, val dateCreated: String,
+                         val ownerName: String, val ownerAvatar: String,
+                         val isBookmaked: Boolean)
